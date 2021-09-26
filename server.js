c52cf426a54b7c6db89c6d6fea025233d2ff93b3
Kiril Abaskin <kiril.abashkin@gmail.com>
2021-09-26 22:49:36 +0300

    prettify file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = 5000;
 
-app.use(express.static('dist/'));
+app.use(express.static("dist/"));
 
-app.get('*', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
+app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
